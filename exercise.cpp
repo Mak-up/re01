795525ccabcb13d1afc77252146f5fe7147a9b3a
maikinw <1657874344@qq.com>
2022-06-13 10:59:04 +0800

    change

---
+++ exercise.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define x first
+#define y second
+#define all(x) x.begin(),x.end()
+#define rep(i, l, r) for (int i = l; i <= r; i++)
+#define nep(i, r, l) for (int i = r; i >= l; i--)
+using namespace std;
+typedef pair<int,int> PII;
+typedef pair<pair<int,int>,int> PIII;
+typedef long long LL;
+typedef unsigned long long ULL;
+const int INF=0x3f3f3f3f,MOD=1000000007,P=131;
+int n,m;
+
+int main()
+{
+cout<<"hallo_word";
+
+  return 0;
+}
