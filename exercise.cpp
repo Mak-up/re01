3b736d44f12b28672d63c6aa5cb3aea2fdbd840b
maikinw <1657874344@qq.com>
2022-06-19 20:12:20 +0800

    change

--- exercise.cpp
+++ exercise.cpp
@@ -1,75 +1,27 @@
-#include "stdio.h"
-#include "stdlib.h"
-#define TRUE  1
-#define FALSE  0
-#define OK  1
-#define ERROR  0
-#define INFEASLBLE  -1
-#define OVERFLOW  -2
-#define MAXSTRLEN  255 	//用户可在255以内定义最大串长
-typedef unsigned char SString[MAXSTRLEN+1];	//0号单元存放串的长度
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <vector>
+#define x first
+#define y second
+#define all(x) x.begin(),x.end()
+#define rep(i, l, r) for (int i = l; i <= r; i++)
+#define nep(i, r, l) for (int i = r; i >= l; i--)
+using namespace std;
+typedef pair<int,int> PII;
+typedef pair<pair<int,int>,int> PIII;
+typedef long long LL;
+typedef unsigned long long ULL;
+const int INF=0x3f3f3f3f,MOD=1000000007,P=131;
+int n,m;
 
-void get_next(SString T,int next[]){
-// 算法4.7
-// 求模式串T的next函数值并存入数组next
-   // 请补全代码
-   int i=1,j=0;
-   while(T[i]!='\0')
-   {
-    if(T[i]==T[j]||j==0)
-      next[++i]= ++j;
-    else j = next[j];
-   }
-
-
-
-
-}
-
-int Index_KMP(SString S,SString T,int pos){
-// 算法4.6
-// 利用模式串T的next函数求T在主串S中第pos个字符之后的位置
-// KMP算法。请补全代码
-int next[MAXSTRLEN+1];
-get_next(T,next);
-int i  = pos,j=0;
-while(i<=S[0]&&j<=T[0])
-{
-  if(S[i]==T[j]||j==0)
-   i++,j++;
-   else j = next[j];
-}
-if(j>T[0]) return i-T[0];
-
-
-
-}
 int main()
-{
-SString T,S;
- int i,j,n;
- char ch;
- int pos;
- scanf("%d",&n);    // 指定n对需进行模式匹配的字符串
-ch=getchar();
-for(j=1;j<=n;j++)
-{
-ch=getchar();
-  for( i=1;i<=MAXSTRLEN&&(ch!='\n');i++)    // 录入主串
-  {
-S[i]=ch;
-  ch=getchar();
-}
-S[0]=i-1;    // S[0]用于存储主串中字符个数
-ch=getchar();
-for( i=1;i<=MAXSTRLEN&&(ch!='\n');i++)    // 录入模式串
-{
-  T[i]=ch;
-  ch=getchar();
-}
-T[0]=i-1;    // T[0]用于存储模式串中字符个数
-pos=       Index_KMP( S, T,0) ;   // 请填空
-printf("%d\n",pos);
-}
-return 0;
-    }
+{ 
+  cout<<"hallo word";
+  cout << "hall"dfs;
+
+  return 0;
+} 
